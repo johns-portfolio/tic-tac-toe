90b5052416d228511d4079d49a8658570e6131e2
Apisit Sianglert <apisit.sianglert@lotuss.com>
2022-08-19 11:43:14 +0700

    Fix static server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import express from 'express'
 import * as dotenv from 'dotenv'
-import StaticServer from 'static-server'
 dotenv.config()
 
-const server = new StaticServer({
-  rootPath: 'dist',
-  port: process.env.PORT
-})
+const app = express()
+const port = +process.env.PORT
 
-server.start(function () {
-  console.log('Server listening to', server.port)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const publicPath = path.join(__dirname, 'dist')
+app.use(express.static(publicPath))
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`)
 })
